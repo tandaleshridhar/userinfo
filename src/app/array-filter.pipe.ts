ea374670e7683602cf5f27912dea6564e74f7986
tandaleshridhar <tandaleshridhar27@gmail.com>
2020-02-14 00:19:06 +0530

    search completed

--- src/app/array-filter.pipe.ts
+++ src/app/array-filter.pipe.ts
@@ -5,7 +5,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ArrayFilterPipe implements PipeTransform {
 
-  /*
+
   transform(items: any[], filter: string): any[] {
     if (!items || !filter) {
       return items;
@@ -14,17 +14,7 @@ export class ArrayFilterPipe implements PipeTransform {
     // filter items array, items which match and return true will be
     // kept, false will be filtered out
     return items.filter(item => {
-      return (item && item.FirstName && item.FirstName.toLowerCase().indexOf(filter.toLowerCase()) > -1);
-    });
-  }
-  */
-
-  transform(items: any[], receivedSearchText: string): any[] {
-    if (!items) return [];
-    if (!receivedSearchText) return items;
-    receivedSearchText = receivedSearchText.toString().toLowerCase();
-    return items.filter(it => {
-      return it.toString().toLowerCase().includes(receivedSearchText);
+      return (item && item.DisplayName && item.DisplayName.toLowerCase().indexOf(filter.toLowerCase()) > -1);
     });
   }
 }
